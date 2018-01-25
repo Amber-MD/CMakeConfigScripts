a35166e1b4cdbbcda8dc55f60c327c45b0dbbdbe
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2018-01-25 01:44:02 -0800

    RPATHConfig.cmake: fix typo

--- RPATHConfig.cmake
+++ RPATHConfig.cmake
@@ -10,7 +10,7 @@ if(TARGET_OSX)
 	set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 else()
 	
-	# set the RPATH to the absolute install dir.  This enables using many Amber programs without sourcign amber.sh.
+	# set the RPATH to the absolute install dir.  This enables using many Amber programs without sourcing amber.sh.
 	# If you do move the install tree, then amber.sh will still set LD_LIBRARY_PATH, and it'll will work fine once you've sourced it.
 	set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIBDIR}")
 	set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
